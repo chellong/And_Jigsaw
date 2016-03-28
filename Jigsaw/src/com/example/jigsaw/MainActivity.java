5731979d8888fc58169d296818204ddb18d2861a
晓龙 <352977335@qq.com>
2016-03-28 23:43:32 +0800

    16,03,28,00
    项目创建

---
+++ Jigsaw/src/com/example/jigsaw/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jigsaw;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
